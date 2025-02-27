b5db18cce4d3d7161e88db848a135113a6552102
Sourabh Jaiswal <124076587+Sourabhjaiswal12016@users.noreply.github.com>
2025-02-27 13:00:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://pern-clone-frontend.onrender.com",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true, 
+  }));
 app.use(express.json());
 
 // Get all Restaurants
